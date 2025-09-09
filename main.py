a129a3b5c79d6974f0d71d75c6cad611b3b56da7
yasinskislava <109846905+yasinskislava@users.noreply.github.com>
2025-09-09 12:37:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def sum(n, m):
-    print("The sum of these numbers is", n+m)
-
+    return n+m
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 
-sum(a,b)
+print("The sum is" ,sum(a,b))
